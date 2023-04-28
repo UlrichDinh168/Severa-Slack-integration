26fce68ed72aa7e05d3a8c3c0206ae0b457ea925
__ud≈∞ <duong0028@gmail.com>
2023-04-28 13:21:13 +0300

    Update constant.js

--- utils/constant.js
+++ utils/constant.js
@@ -1,12 +1,12 @@
-export const EXCLUDED_PROJECT_LIST = ["MC Marketing 2021", "MC Marketing 2022"];
+export const EXCLUDED_PROJECT_LIST = ["XXX", "YYY"];
 
 export const EXCLUDED_PERSON_LIST = [
   "b4f6735f-8fd2-75e7-f125-31ef8f4a8ba9",
   "674d9d71-7753-bb5f-c05c-7603bbea8890",
   "4a9ad222-d8cd-dcb6-5cea-bcf040bf6560",
   "b0dfb6a5-15e1-0bfb-c5ae-8023fa70c0c9",
-  "52275fef-9479-7c40-bfa5-2838567f9872", // Jari Hirvonen
-  "14601dff-49db-dcef-861a-f7b03bf1984d", // Live Haraldsen
-  '40b3667d-9f7c-9eaf-1ec1-9f16abbc29ee', // Eero Jantunen
-  '05d5aa62-dc14-95e1-864f-5558202dfadb' // Mary Meinander
+  "52275fef-9479-7c40-bfa5-2838567f9872", 
+  "14601dff-49db-dcef-861a-f7b03bf1984d", 
+  '40b3667d-9f7c-9eaf-1ec1-9f16abbc29ee', 
+  '05d5aa62-dc14-95e1-864f-5558202dfadb' 
 ];
